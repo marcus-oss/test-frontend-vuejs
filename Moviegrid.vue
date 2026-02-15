64a7792ae6b35398157c97354e180a92b5a79119
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2026-02-15 13:51:49 -0300

    Add Moviegrid component to display movie cards

---
+++ Moviegrid.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MovieCard from './MovieCard.vue';
+import { useMovieStore } from '@/stores/movieStore';
+
+const store = useMovieStore();
+</script>
+
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <MovieCard 
+      v-for="movie in store.movies" 
+      :key="movie.imdbID" 
+      :movie="movie"
+      :is-favorite="store.isFavorite(movie.imdbID)"
+      @toggle-favorite="store.toggleFavorite"
+    />
+  </div>
+</template>
